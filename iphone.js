8f4f4be287375f81781ba73ddeabc63ed07485bb
khalil <charif.khalil1@gmail.com>
2022-10-24 00:01:43 +0100

    new upload

---
+++ iphone.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+let navbar = document.querySelector('.navbar')
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background = color;
+}
